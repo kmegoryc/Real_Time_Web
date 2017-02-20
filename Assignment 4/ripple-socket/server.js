189b2ad798695c9b40cd5403089c8b51722f9efd
Keren Megory-Cohen <keren.megorycohen@colorado.edu>
2017-02-20 08:52:22 -0700

    Add Assignment 4: Ripple Socket

---
+++ Assignment 4/ripple-socket/server.js
@@ -0,0 +1,46 @@
+// HTTP PORTION
+var http = require('http');
+var fs = require('fs');
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	console.log("The Request is: " + parsedUrl.pathname);
+		
+	fs.readFile(__dirname + parsedUrl.pathname, 
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	);
+  	
+}
+
+
+// WEBSOCKET PORTION
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection', 
+
+	function (socket) {
+	
+		console.log("We have a new client: " + socket.id);
+		
+		socket.on('drawing', function(data) {
+			//send this drawing data to EVERYONE but myself (aka don't send back to the sender/client it came from)
+			socket.broadcast.emit('otherdrawing', data);
+		});
+
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
---
+++ Assignment 4/ripple-socket/sketch.js
@@ -0,0 +1,70 @@
+var ripples = []; // empty array named ripples
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+}
+
+function draw() {
+  background(71, 134, 237);
+  // loop through all ripples objects and call its methods
+  for (var i = 0; i < ripples.length; i++) {
+    ripples[i].grow();
+    ripples[i].display();
+  }
+}
+
+function mousePressed() {
+  // adds a Ripple to the ripples array
+  ripples.push(new Ripple(mouseX, mouseY));
+  // when the mouse is pressed, send Ripple data 
+  sendRipple(
+    {
+    'x': mouseX,
+    'y': mouseY
+    }
+    );
+}
+
+function Ripple(xin, yin) {
+  
+  this.x = xin;
+  this.y = yin;
+  this.diam = 1;
+  this.a = 255;
+  
+  this.grow = function() {
+    this.a-=random(0, 1);
+    // grow ripple size at decelerating rate
+    for(var g = 1.0; g > 0.1; g--){
+      this.diam+=g;
+    }
+  }
+  
+  this.display = function() {
+    // draw ripples to screen
+    stroke(255, 255, 255, this.a);
+    strokeWeight(2);
+    ellipse(this.x, this.y, this.diam, this.diam);
+    strokeWeight(1.5);
+    ellipse(this.x, this.y, this.diam+30, this.diam+30);
+    strokeWeight(3);
+    ellipse(this.x, this.y, this.diam+50, this.diam+50); 
+    strokeWeight(1);
+    ellipse(this.x, this.y, this.diam+20, this.diam+20);
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function sendRipple(message) {
+  socket.emit('drawing', message);
+}
+
+function otherDraw(dataX, dataY) {
+  // add Ripple to the ripples array for the new client(s)
+  ripples.push(new Ripple(dataX, dataY));
+}
+
